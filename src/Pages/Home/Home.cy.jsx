8907a2c50b60a17c45c94a66df073093531914d9
Mohammed <68031021+MohammedJava@users.noreply.github.com>
2023-01-31 20:19:24 -0500

    integration test for the home page

--- src/Pages/Home/Home.cy.jsx
+++ src/Pages/Home/Home.cy.jsx
@@ -6,4 +6,13 @@ describe("<Home />", () => {
     // see: https://on.cypress.io/mounting-react
     cy.mount(<Home />);
   });
+
+  beforeEach(() => {
+    cy.mount(<Home />);
+  });
+
+  it("changes route when clicking on the sign in button", () => {
+    cy.get('[data-testid="homeLink"]').click();
+    cy.url().should("be.equal", "http://localhost:8080/signin");
+  });
 });
