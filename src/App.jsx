cd9b96e43b1542eda73f00d5159475e66b99b5ee
Ryan Kim <rck2001@hotmail.com>
2023-02-06 22:54:49 -0500

    Revert "added accept invitation and view network pages"
    This reverts commit d0c9ed21b2fb2e687b2cae381ff833bff1e8d7e2.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable prettier/prettier */
 import React from "react";
 import {
   BrowserRouter as Router,
@@ -14,8 +13,6 @@ import AnotherPage from "./Pages/AnotherPage/AnotherPage";
 import SignIn from "./Pages/SignIn/SignIn";
 import SignUp from "./Pages/SignUp/SignUp";
 import AccountCreation from "./Pages/AccountCreation/AccountCreation";
-import ViewNetwork from "./Pages/Network/ViewNetwork";
-import AcceptInvitation from "./Pages/Network/Invitations/AcceptInvitation";
 
 const App = () => {
   const hello = "hello";
@@ -32,8 +29,6 @@ const App = () => {
           <Route path="/signin" exact element={<SignIn />} />
           <Route path="/signup" exact element={<SignUp />} />
           <Route path="/accountCreation" exact element={<AccountCreation />} />
-          <Route path="/viewNetwork" exact element={<ViewNetwork />} />
-          <Route path="/acceptinvitation" exact element={<AcceptInvitation />} />
         </Routes>
       </Router>
     </div>
