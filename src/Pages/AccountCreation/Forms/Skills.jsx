ae5496309d3dbea13320f809439aa422c8d73ef3
Its-Alann <76508163+Its-Alann@users.noreply.github.com>
2023-02-07 03:03:37 -0500

    Skills submits form data

--- src/Pages/AccountCreation/Forms/Skills.jsx
+++ src/Pages/AccountCreation/Forms/Skills.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
+import PropTypes from "prop-types";
 
-const Skills = () => (
+const Skills = ({ setSkills, values }) => (
   <Grid
     container
     spacing={0}
@@ -18,9 +19,18 @@ const Skills = () => (
         label="Description"
         multiline
         rows={6}
+        value={values.skills}
+        onChange={(e) => setSkills(e.target.value)}
       />
     </Grid>
   </Grid>
 );
+
+Skills.propTypes = {
+  setSkills: PropTypes.func,
+  values: PropTypes.shape({
+    skills: PropTypes.string,
+  }),
+};
 
 export default Skills;
