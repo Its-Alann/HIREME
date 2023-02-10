b21c77990673704cb0d27b8d2f2961d1d8472581
Ryan Kim <rck2001@hotmail.com>
2023-02-09 23:44:20 -0500

    stashing changes before pulling from main

--- src/Pages/Network/MyConnections/ViewNetwork.jsx
+++ src/Pages/Network/MyConnections/ViewNetwork.jsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
+import { collection } from "firebase/firestore";
 import Navbar from "../../../Components/Navbar/Navbar";
 import NetworkCards from "../../../Components/Network/NetworkCards";
+import { db } from "../../../Firebase/firebase";
 
 const theme = createTheme();
 const test = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+const usersRef = collection(db, "userProfiles");
 
 const ViewNetwork = () => (
   <ThemeProvider theme={theme}>
@@ -22,7 +25,7 @@ const ViewNetwork = () => (
       <Box justifyContent="center" alignItems="center" minHeight="60vh">
         {/*The array will contain all the connected users*/}
         <Grid container spacing={3}>
-          {Array.from(test).map((_, index) => (
+          {Array.from(usersRef).map((_, index) => (
             <Grid item>
               <NetworkCards />
             </Grid>
