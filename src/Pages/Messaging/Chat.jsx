94da3043b2996e146f18dc40d3b235d7e351f4b4
georgia1515 <georgiapitic@gmail.com>
2023-02-10 00:58:08 -0500

    query user sender for loop issue

--- src/Pages/Messaging/Chat.jsx
+++ src/Pages/Messaging/Chat.jsx
@@ -28,26 +28,73 @@ import SendChatButton from "../../Components/SendChatButton/SendChatButton";
 import { db, auth } from "../../Firebase/firebase";
 
 const theme = createTheme();
+// get our sender s email
+//const user = auth.currentUser.email;
+//
+const user = "HMa7dZP4QoNZkpcl5Mpgi7vT5Vh1";
+console.log(user);
 
 const Chat = () => {
   const [messages, setMessages] = useState([]);
+  const [profiles, setProfiles] = useState([]);
   const messagesRefProfile = collection(db, "userProfiles");
   const messagesRef = collection(db, "messages");
+  const receivers = [];
+
+  const [userToPrint, setUserToPrint] = useState([]);
 
   useEffect(() => {
-    // Get messages from Firestore
+    // get receiver's email from messages collection
+
     const queryMessages = query(messagesRef);
-    const unsuscribe = onSnapshot(queryMessages, (snapshot) => {
+    let unsuscribe = onSnapshot(queryMessages, (snapshot) => {
       const msgs = [];
       snapshot.forEach((doc) => {
         msgs.push({ ...doc.data(), id: doc.id });
       });
-      console.log(msgs);
-      // console.log(msgs[2].values.firstName);
-      // console.log(msgs[0].messages[1].content.messageContent);
 
-      setMessages(msgs[0].messages[1].content.messageContent);
-      // setMessages(msgs[0].messages[1].content.messageContent);
+      //console.log(msgs.length);
+
+      //for (let i = 0; i < msgs.length; ) {
+      //msgs.forEach((el, index, array) => {});
+      setMessages(msgs[0].id);
+      console.log(messages);
+
+      if (messages.includes(user)) {
+        const arrayOfIds = messages.split("-");
+        console.log(arrayOfIds[1]);
+        setUserToPrint(messages);
+        console.log("The search string  was found in ");
+      } else {
+        console.log(messages);
+        const arrayOfIds = messages.split("-");
+        console.log(arrayOfIds[0]);
+
+        if (arrayOfIds[0] === user) {
+          setUserToPrint(arrayOfIds[1]);
+        } else {
+          //console.log(arrayOfIds[0]);
+          setUserToPrint(arrayOfIds[0]);
+          //console.log(userToPrint);
+        }
+        //
+        console.log(userToPrint);
+      }
+      //i += 1;
+      console.log(1);
+      //}
+    });
+    console.log(userToPrint);
+    // get receiver's name from userProfile collection
+    const queryUser = query(messagesRefProfile);
+    unsuscribe = onSnapshot(queryUser, (snapshot) => {
+      const profs = [];
+      snapshot.forEach((doc) => {
+        profs.push({ ...doc.data(), id: doc.id });
+      });
+      console.log(profs[3].values.firstName);
+
+      setProfiles(profs[3].values.firstName);
     });
 
     return () => unsuscribe();
@@ -57,7 +104,11 @@ const Chat = () => {
     <ThemeProvider theme={theme}>
       <Navbar />
       <div>
-        <p>hello {messages}</p>
+        {receivers}
+        {/* <p>hello {messages}</p>
+        <p>hello {user}</p>
+        <p>hello {userToPrint}</p> */}
+        <p>{profiles}</p>
       </div>
     </ThemeProvider>
   );
