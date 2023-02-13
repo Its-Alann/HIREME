b7599ff4b2939c4b28cab133766bb9e104280242
Aykchen <aliceykchen01@gmail.com>
2023-02-13 02:04:41 -0500

    Display names on left side of Messaging

--- src/Components/Messaging/ConnectionList.jsx
+++ src/Components/Messaging/ConnectionList.jsx
@@ -1,23 +1,76 @@
 import * as React from "react";
+import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
+import { collection, getDocs } from "firebase/firestore";
 import ConnectionListItem from "./ConnectionListItem";
+import { db, auth } from "../../Firebase/firebase";
+
+// get our sender s email
+//const user = auth.currentUser.email;
+//
+const user = "HMa7dZP4QoNZkpcl5Mpgi7vT5Vh1";
+const messagesRefProfile = collection(db, "userProfiles");
+const messagesRef = collection(db, "messages");
 
 const ConnectionList = (props) => {
+  const [dmList, setDmList] = useState([]);
+  const [profiles, setProfiles] = useState([]);
+
+  // get all names of user's receivers
+  const getAllReceivers = async () => {
+    let querySnapshot = await getDocs(collection(db, "messages"));
+    const allAuthors = [];
+    querySnapshot.forEach((doc) => {
+      // If the user is a participant in the conversation
+      //  then add the rest of the participants
+      if (doc.data().authors.includes(user)) {
+        const recipients = doc
+          .data()
+          .authors.filter((author) => author !== user);
+
+        allAuthors.push(recipients);
+      }
+
+      setDmList(allAuthors);
+    });
+
+    querySnapshot = await getDocs(collection(db, "userProfiles"));
+    const allUsers = [];
+    querySnapshot.forEach((doc) => {
+      // If the user is a participant in the conversation
+      //  then add the name of the participants
+      const userID = doc.id;
+      allAuthors.forEach((el) => {
+        if (el[0] === userID) {
+          allUsers.push(
+            `${doc.data().values.firstName} ${doc.data().values.lastName}`
+          );
+        }
+        setProfiles(allUsers);
+      });
+    });
+  };
+
+  useEffect(() => {
+    // display automatically the names of the user's receivers
+    getAllReceivers();
+  }, []);
+
   const hello = "hello";
   return (
     <List>
-      {props.connections.map((connection) => (
-        <ListItem button key={connection}>
-          <ConnectionListItem UID={connection} />
+      {profiles.map((el) => (
+        <ListItem button key={el}>
+          {el}
         </ListItem>
       ))}
     </List>
   );
 };
 
-ConnectionList.propTypes = {
-  connections: PropTypes.arrayOf(PropTypes.any),
-};
+// ConnectionList.propTypes = {
+//   connections: PropTypes.arrayOf(PropTypes.any),
+// };
 export default ConnectionList;
