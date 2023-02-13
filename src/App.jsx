30d962957ee782bad256d18ba8ccae366d19b2a0
Alan Vadlakunta <76508163+Its-Alann@users.noreply.github.com>
2023-02-13 14:40:22 -0500

    Started Edit Profile

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import AnotherPage from "./Pages/AnotherPage/AnotherPage";
 import Login from "./Pages/Login/Login";
 import SignUp from "./Pages/SignUp/SignUp";
 import AccountCreation from "./Pages/AccountCreation/AccountCreation";
+import EditProfile from "./Pages/EditProfile/EditProfile";
 
 const App = () => {
   const hello = "hello";
@@ -29,6 +30,7 @@ const App = () => {
           <Route path="/login" exact element={<Login />} />
           <Route path="/signup" exact element={<SignUp />} />
           <Route path="/accountCreation" exact element={<AccountCreation />} />
+          <Route path="/editProfile" exact element={<EditProfile />} />
         </Routes>
       </Router>
     </div>
