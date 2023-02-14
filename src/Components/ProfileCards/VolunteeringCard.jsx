0f02062f6d83a2840a3a03c85be2a60cd1ee427d
Its-Alann <76508163+Its-Alann@users.noreply.github.com>
2023-02-14 01:16:34 -0500

    Added profile volunteering card

---
+++ src/Components/ProfileCards/VolunteeringCard.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Grid, Box, Card, CardContent, Typography } from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+
+const VolunteeringCard = () => {
+  const something = "";
+  return (
+    <Box>
+      <Card variant="outlined" sx={{ mx: 5 }}>
+        <CardContent>
+          <Grid container justifyContent="space-between">
+            <Grid item>
+              <Typography variant="h5"> Volunteering </Typography>
+            </Grid>
+            <Grid item>
+              <EditIcon />
+            </Grid>
+          </Grid>
+          <Grid container spacing={3}>
+            <Grid item>
+              <Typography variant="body2"> Organization </Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="body2"> May 2018 </Typography>
+            </Grid>
+          </Grid>
+          <Grid container spacing={3}>
+            <Grid item>
+              <Typography variant="body2">
+                {" "}
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+                maximus nibh nec massa convallis, quis bibendum neque fringilla.
+                Mauris et odio a sem malesuada fermentum. Nunc sed lacus sit
+                amet augue hendrerit tristique viverra non enim. Aliquam vitae
+                porta neque. Nullam sodales nibh at velit pulvinar, in fermentum
+                dolor dapibus. Donec efficitur metus ultricies magna mattis,
+                eget vulputate tortor hendrerit. In in tempus neque. Fusce urna
+                enim, faucibus quis bibendum ut, dictum quis urna. Aenean
+                viverra urna tellus, vel vestibulum nulla finibus quis. Sed
+                dolor tortor, iaculis ut purus id, dignissim sodales quam.
+                Aliquam consectetur vitae mi in lacinia. In in ante ac nisi
+                faucibus volutpat eu sed purus. Mauris commodo turpis ac augue
+                condimentum, id tempor nunc tincidunt. In scelerisque mi id
+                ullamcorper faucibus.{" "}
+              </Typography>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+    </Box>
+  );
+};
+
+export default VolunteeringCard;
