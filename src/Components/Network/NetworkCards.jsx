f81cffc797526276d3baf3ebf10682d04f6cfbc4
RIGNITE <rck2001@hotmail.com>
2023-02-15 15:12:25 -0500

    pulling the associated users based on the network table

--- src/Components/Network/NetworkCards.jsx
+++ src/Components/Network/NetworkCards.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/function-component-definition */
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
@@ -9,6 +9,16 @@ import Avatar from "@mui/material/Avatar";
 import { PropTypes } from "prop-types";
 import { styled } from "@mui/material/styles";
 import { blue } from "@mui/material/colors";
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  getDoc,
+  doc,
+} from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { db, auth } from "../../Firebase/firebase";
 
 const ColorButtonBlue = styled(Button)(({ theme }) => ({
   color: theme.palette.getContrastText(blue[600]),
@@ -26,55 +36,72 @@ const ColorButtonLightBlue = styled(Button)(({ theme }) => ({
   },
 }));
 
-const NetworkCards = ({
-  userImage,
-  userFirstName,
-  userLastName,
-  userBio,
-  userID,
-}) => (
-  <Box sx={{ width: 300, minWidth: 100 }}>
-    <Card variant="outlined">
-      <>
-        <CardHeader
-          avatar={
-            //source will be the user's image
-            <Avatar
-              aria-label="user"
-              sx={{ width: 56, height: 56 }}
-              src={userImage}
+export const NetworkCards = ({ connectedUserID }) => {
+  const [connectedUser, setConnectedUser] = useState([]);
+
+  useEffect(() => {
+    const getConnectedUsers = async () => {
+      try {
+        //console.log(userID);
+        const docSnap = await getDoc(doc(db, "userProfiles", connectedUserID));
+        const userData = docSnap.data();
+        setConnectedUser(userData);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    getConnectedUsers();
+  }, []);
+
+  return (
+    <div>
+      <Box sx={{ width: 300, minWidth: 100 }}>
+        <Card variant="outlined">
+          <>
+            <CardHeader
+              avatar={
+                //source will be the user's image
+                <Avatar
+                  aria-label="user"
+                  sx={{ width: 56, height: 56 }}
+                  src={connectedUser.values.image}
+                />
+              }
+              //title will be the user's name and subheader is their bio
+              title={
+                connectedUser.values.firstName !== "" &&
+                connectedUser.values.lastName !== ""
+                  ? `${connectedUser.values.firstName} ${connectedUser.values.lastName}`
+                  : "No name"
+              }
+              subheader={
+                //remove != null when incomplete users are removed
+                connectedUser.values.description !== "" &&
+                connectedUser.values.description != null
+                  ? `${connectedUser.values.description}`
+                  : "No bio"
+              }
             />
-          }
-          //title will be the user's name and subheader is their bio
-          title={
-            userFirstName !== "" && userLastName !== ""
-              ? `${userFirstName} ${userLastName}`
-              : "No name"
-          }
-          subheader={
-            //remove != null when incomplete users are removed
-            userBio !== "" && userBio != null ? `${userBio}` : "No bio"
-          }
-        />
-        {/*moves the buttons to the right*/}
-        <Box display="flex" justifyContent="flex-end">
-          <CardActions>
-            {/*view profile will go to the user's profile and message will be sent to the */}
-            <ColorButtonBlue size="medium">View Profile</ColorButtonBlue>
-            <ColorButtonLightBlue size="medium">Message</ColorButtonLightBlue>
-          </CardActions>
-        </Box>
-      </>
-    </Card>
-  </Box>
-);
+            {/*moves the buttons to the right*/}
+            <Box display="flex" justifyContent="flex-end">
+              <CardActions>
+                {/*view profile will go to the user's profile and message will be sent to the */}
+                <ColorButtonBlue size="medium">View Profile</ColorButtonBlue>
+                <ColorButtonLightBlue size="medium">
+                  Message
+                </ColorButtonLightBlue>
+              </CardActions>
+            </Box>
+          </>
+        </Card>
+      </Box>
+    </div>
+  );
+};
 
 NetworkCards.propTypes = {
-  userImage: PropTypes.string.isRequired,
-  userFirstName: PropTypes.string.isRequired,
-  userLastName: PropTypes.string.isRequired,
-  userBio: PropTypes.string.isRequired,
-  userID: PropTypes.string.isRequired,
+  connectedUserID: PropTypes.string.isRequired,
 };
 
 export default NetworkCards;
