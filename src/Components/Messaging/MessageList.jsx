02e9c19af3275660af75b9ffa17298bff66d0848
ryanwong2000 <ryanwong2000@hotmail.com>
2023-02-15 01:43:22 -0500

    RENAMING

--- src/Components/Messaging/MessageList.jsx
+++ src/Components/Messaging/MessageList.jsx
@@ -3,7 +3,7 @@ import * as React from "react";
 import PropTypes from "prop-types";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
-import MessageListItem from "./ConversationListItem";
+import MessageListItem from "./MessageListItem";
 
 const MessageList = (props) => {
   // Very mysterious
