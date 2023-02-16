e61b8bd7681b599eabe06e8df9c0544761bd7ec2
Its-Alann <76508163+Its-Alann@users.noreply.github.com>
2023-02-16 03:42:19 -0500

    Education card to db complete

--- src/Components/ProfileCards/EducationCard.jsx
+++ src/Components/ProfileCards/EducationCard.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import { React, useEffect, useState } from "react";
 import { Grid, Box, Card, CardContent, Typography, Chip } from "@mui/material";
 import EditIcon from "@mui/icons-material/Edit";
+import PropTypes from "prop-types";
 
-const EducationCard = () => {
-  const courses = [
-    "Data Structures",
-    "Databases",
-    "Object Oriented Programming",
-  ];
+const EducationCard = ({ setProfile, profile, currentUserEmail }) => {
+  const courses = [profile.values.courses];
+  const [startYear, setStartYear] = useState("");
+  const [endYear, setEndYear] = useState("");
+
+  const getDates = async () => {
+    if (profile.values.startDateEdu && profile.values.endDateEdu) {
+      setStartYear(await profile.values.startDateEdu.toDate().getFullYear());
+      setEndYear(await profile.values.endDateEdu.toDate().getFullYear());
+    }
+  };
+
+  useEffect(() => {
+    // console.log("profile", profile);
+    getDates();
+  }, [profile]);
+
   return (
     <Box>
+      {console.log(courses)}
       <Card variant="outlined" sx={{ mx: 5 }}>
         <CardContent>
           <Grid container justifyContent="space-between">
@@ -22,17 +35,20 @@ const EducationCard = () => {
           </Grid>
           <Grid container spacing={3}>
             <Grid item>
-              <Typography variant="body2"> Concordia University </Typography>
+              <Typography variant="body2"> {profile.values.school} </Typography>
             </Grid>
             <Grid item>
-              <Typography variant="body2"> 2020 - Present </Typography>
+              <Typography variant="body2">
+                {" "}
+                {startYear} - {endYear}{" "}
+              </Typography>
             </Grid>
           </Grid>
           <Grid container spacing={3}>
             <Grid item>
               <Typography variant="body2">
                 {" "}
-                Bachelor of Engineering, Software Engineering{" "}
+                {profile.values.degree}, {profile.values.program}
               </Typography>
             </Grid>
           </Grid>
@@ -47,6 +63,12 @@ const EducationCard = () => {
       </Card>
     </Box>
   );
+};
+
+EducationCard.propTypes = {
+  profile: PropTypes.objectOf(PropTypes.any),
+  setProfile: PropTypes.func,
+  currentUserEmail: PropTypes.string,
 };
 
 export default EducationCard;
