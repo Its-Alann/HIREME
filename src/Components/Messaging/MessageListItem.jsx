15d6bea2f0b2c4dc8f0b5fff65fdcb264e62ab3f
Aykchen <aliceykchen01@gmail.com>
2023-02-16 22:59:41 -0500

    Updated message text section
    Co-authored-by: Georgia Pitic <georgia1515@users.noreply.github.com>
    Co-authored-by: Ryan Wong <ryanwong2000@users.noreply.github.com>

--- src/Components/Messaging/MessageListItem.jsx
+++ src/Components/Messaging/MessageListItem.jsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import PropTypes from "prop-types";
 import Grid from "@material-ui/core/Grid";
+import Stack from "@mui/material/Stack";
 import ListItemText from "@material-ui/core/ListItemText";
 import { Timestamp } from "firebase/firestore";
 import { auth } from "../../Firebase/firebase";
@@ -15,21 +16,55 @@ const MessageListItem = ({ content, sender, timestamp }) => {
   }, [auth]);
 
   return (
-    <Grid container>
-      <Grid item xs={12}>
+    <Stack
+      container
+      sx={{ display: "inline-block" }}
+      style={{ float: `${alignment}` }}
+    >
+      <Grid style={{ width: "100%" }}>
         <ListItemText
+          secondary={sender}
           align={alignment}
+          // sx={{ ml: 12 }}
+          style={{
+            marginLeft: "12px",
+            marginRight: "12px",
+            marginTop: "12px",
+            maxWidth: "100%",
+            // display: "block",
+            // backgroundColor: "#D9D9D9",
+          }}
+        />
+      </Grid>
+
+      <Grid
+        item
+        xs={4}
+        style={{
+          backgroundColor: alignment === "right" ? "yellow" : "lime",
+          maxWidth: "100%",
+          padding: "12px",
+          borderRadius: "16px",
+          // display: "inline-flex",
+          // wordBreak: "break-all",
+          // overflowWrap: "break-word",
+          // float: `${alignment}`,
+        }}
+      >
+        <ListItemText
+          sx={{ display: "inline-block" }}
+          // align="left"
           primary={content}
-          style={{ backgroundColor: "#D9D9D9" }}
-        />
-      </Grid>
-      <Grid item xs={12}>
-        <ListItemText
           align={alignment}
-          secondary={timestamp.toLocaleString()}
+          // float={alignment}
         />
       </Grid>
-    </Grid>
+      <ListItemText
+        style={{ marginLeft: "12px" }}
+        align={alignment}
+        secondary={timestamp.toLocaleString()}
+      />
+    </Stack>
   );
 };
 
