9dca3ba596a5e562ea80141729129df2d17e2205
Its-Alann <76508163+Its-Alann@users.noreply.github.com>
2023-02-18 13:41:04 -0500

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -27,36 +27,36 @@ module.exports = defineConfig({
       codeCoverageTasksRegistered: true,
     },
 
-    devServer: {
-      framework: "create-react-app",
-      bundler: "webpack",
-      webpackConfig: {
-        mode: "development",
-        devtool: false,
-        module: {
-          rules: [
-            // application and Cypress files are bundled like React components
-            // and instrumented using the babel-plugin-istanbul
-            // (we will filter the code coverage for non-application files later)
-            {
-              test: /\.js/,
-              exclude: /node_modules/,
-              use: {
-                loader: "babel-loader",
-                options: {
-                  presets: ["@babel/preset-env", "@babel/preset-react"],
-                  plugins: [
-                    // we could optionally insert this plugin
-                    // only if the code coverage flag is on
-                    "istanbul",
-                  ],
-                },
-              },
-            },
-          ],
-        },
-      },
-    },
+    // devServer: {
+    //   framework: "create-react-app",
+    //   bundler: "webpack",
+    //   webpackConfig: {
+    //     mode: "development",
+    //     devtool: false,
+    //     module: {
+    //       rules: [
+    //         // application and Cypress files are bundled like React components
+    //         // and instrumented using the babel-plugin-istanbul
+    //         // (we will filter the code coverage for non-application files later)
+    //         {
+    //           test: /\.js/,
+    //           exclude: /node_modules/,
+    //           use: {
+    //             loader: "babel-loader",
+    //             options: {
+    //               presets: ["@babel/preset-env", "@babel/preset-react"],
+    //               plugins: [
+    //                 // we could optionally insert this plugin
+    //                 // only if the code coverage flag is on
+    //                 "istanbul",
+    //               ],
+    //             },
+    //           },
+    //         },
+    //       ],
+    //     },
+    //   },
+    // },
     specs: ["./components"],
   },
 
