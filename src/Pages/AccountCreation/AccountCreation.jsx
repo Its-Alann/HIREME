a855a2f75032f9c523aceba1a82d74f1c47dffd3
MrMelon1232 <julien.phan18@gmail.com>
2023-02-18 00:16:52 -0500

    added profilePicture feature for account creation

--- src/Pages/AccountCreation/AccountCreation.jsx
+++ src/Pages/AccountCreation/AccountCreation.jsx
@@ -213,8 +213,10 @@ const AccountCreation = () => {
     try {
       const auth = getAuth();
       const user = auth.currentUser;
+      const profilePictureLink = `${user.email}-profilePicture`;
       await setDoc(doc(db, "userProfiles", user.email), {
         values,
+        profilePictureLink,
       });
     } catch (error) {
       console.log(error);
--- src/Pages/AccountCreation/Forms/NameForm.jsx
+++ src/Pages/AccountCreation/Forms/NameForm.jsx
@@ -3,38 +3,47 @@ import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
 import PropTypes from "prop-types";
 import "./Forms.css";
+import ProfilePicture from "../../../Components/ProfileCards/ProfilePicture";
 
 const NameForm = ({ setFirstName, setLastName, values }) => (
   <Grid
-    id="formGrid"
     container
-    spacing={0}
     textAlign="center"
     alignItems="center"
     justifyContent="center"
     style={{ minHeight: "15vh" }}
   >
-    <Grid item xs={12}>
-      <TextField
-        fullWidth
-        required
-        id="standard-required"
-        placeholder="First Name"
-        variant="standard"
-        value={values.firstName}
-        onChange={(e) => setFirstName(e.target.value)}
-      />
-    </Grid>
-    <Grid item xs={12}>
-      <TextField
-        fullWidth
-        required
-        id="standard-required"
-        placeholder="Last Name"
-        variant="standard"
-        value={values.lastName}
-        onChange={(e) => setLastName(e.target.value)}
-      />
+    <ProfilePicture />
+    <Grid
+      id="formGrid"
+      container
+      textAlign="center"
+      alignItems="center"
+      justifyContent="center"
+      style={{ minHeight: "35vh" }}
+    >
+      <Grid item xs={12}>
+        <TextField
+          fullWidth
+          required
+          id="standard-required"
+          placeholder="First Name"
+          variant="standard"
+          value={values.firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <TextField
+          fullWidth
+          required
+          id="standard-required"
+          placeholder="Last Name"
+          variant="standard"
+          value={values.lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+      </Grid>
     </Grid>
   </Grid>
 );
@@ -45,6 +54,7 @@ NameForm.propTypes = {
   values: PropTypes.shape({
     firstName: PropTypes.string.isRequired,
     lastName: PropTypes.string.isRequired,
+    profilePictureUrl: PropTypes.string.isRequired,
   }).isRequired,
 };
 
