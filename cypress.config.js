aaff2dac4463cfa9e7c074804bab3a752e0527b0
Mohammed <68031021+MohammedJava@users.noreply.github.com>
2023-02-18 15:40:27 -0500

    update

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
   video: false,
 
   component: {
-    specPattern: "src/**/*.cy.{js,jsx,ts,tsx}",
+    specPattern: "cypress/__tests__/*.cy.{js,jsx,ts,tsx}",
     setupNodeEvents(on, config) {
       console.log("setupNodeEvents for components");
 
