39131630ae55a4034879aecea7d8a086ede2e110
Its-Alann <76508163+Its-Alann@users.noreply.github.com>
2023-02-18 14:54:26 -0500

    Possible temp fix

--- cypress.config.js
+++ cypress.config.js
@@ -61,6 +61,7 @@ module.exports = defineConfig({
   },
 
   e2e: {
-    // setupNodeEvents(on, config) {},
+    specPattern: "cypress/e2e/*.cy.{js,jsx,ts,tsx}",
+    setupNodeEvents(on, config) {},
   },
 });
