fe16acf948609398cba966085dd14d11cf5562da
Its-Alann <76508163+Its-Alann@users.noreply.github.com>
2023-02-23 01:40:36 -0500

    Fixed missing component tests

--- cypress.config.js
+++ cypress.config.js
@@ -5,61 +5,61 @@ module.exports = defineConfig({
   video: false,
   screenshotOnRunFailure: false,
 
-  // component: {
-  //   specPattern: "cypress/__tests__/ComponentTesting/*.cy.{js,jsx,ts,tsx}",
-  //   setupNodeEvents(on, config) {
-  //     console.log("setupNodeEvents for components");
+  component: {
+    specPattern: "cypress/__tests__/ComponentTesting/*.cy.{js,jsx,ts,tsx}",
+    setupNodeEvents(on, config) {
+      console.log("setupNodeEvents for components");
 
-  //     // https://github.com/bahmutov/cypress-code-coverage
-  //     // eslint-disable-next-line global-require
-  //     require("@bahmutov/cypress-code-coverage/plugin")(on, config);
-  //     // eslint-disable-next-line global-require
-  //     require("@cypress/code-coverage/task")(on, config);
-  //     // eslint-disable-next-line global-require
-  //     on("file:preprocessor", require("@cypress/code-coverage/use-babelrc"));
+      // https://github.com/bahmutov/cypress-code-coverage
+      // eslint-disable-next-line global-require
+      require("@bahmutov/cypress-code-coverage/plugin")(on, config);
+      // eslint-disable-next-line global-require
+      require("@cypress/code-coverage/task")(on, config);
+      // eslint-disable-next-line global-require
+      on("file:preprocessor", require("@cypress/code-coverage/use-babelrc"));
 
-  //     return {
-  //       close: () => {},
-  //       config,
-  //     };
-  //   },
+      return {
+        close: () => {},
+        config,
+      };
+    },
 
-  //   env: {
-  //     codeCoverageTasksRegistered: true,
-  //   },
+    env: {
+      codeCoverageTasksRegistered: false,
+    },
 
-  //   devServer: {
-  //     framework: "create-react-app",
-  //     bundler: "webpack",
-  //     webpackConfig: {
-  //       mode: "development",
-  //       devtool: false,
-  //       module: {
-  //         rules: [
-  //           // application and Cypress files are bundled like React components
-  //           // and instrumented using the babel-plugin-istanbul
-  //           // (we will filter the code coverage for non-application files later)
-  //           {
-  //             test: /\.js/,
-  //             exclude: /node_modules/,
-  //             use: {
-  //               loader: "babel-loader",
-  //               options: {
-  //                 presets: ["@babel/preset-env", "@babel/preset-react"],
-  //                 plugins: [
-  //                   // we could optionally insert this plugin
-  //                   // only if the code coverage flag is on
-  //                   "istanbul",
-  //                 ],
-  //               },
-  //             },
-  //           },
-  //         ],
-  //       },
-  //     },
-  //   },
-  //   specs: ["./components"],
-  // },
+    devServer: {
+      framework: "create-react-app",
+      bundler: "webpack",
+      webpackConfig: {
+        mode: "development",
+        devtool: false,
+        module: {
+          rules: [
+            // application and Cypress files are bundled like React components
+            // and instrumented using the babel-plugin-istanbul
+            // (we will filter the code coverage for non-application files later)
+            {
+              test: /\.js/,
+              exclude: /node_modules/,
+              use: {
+                loader: "babel-loader",
+                options: {
+                  presets: ["@babel/preset-env", "@babel/preset-react"],
+                  plugins: [
+                    // we could optionally insert this plugin
+                    // only if the code coverage flag is on
+                    "istanbul",
+                  ],
+                },
+              },
+            },
+          ],
+        },
+      },
+    },
+    specs: ["./components"],
+  },
 
   e2e: {
     // setupNodeEvents(on, config) {},
