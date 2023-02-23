a204e77fc0b5a1dbb4afff28cb5172a237d7f0d4
Mohammed <68031021+MohammedJava@users.noreply.github.com>
2023-02-23 09:03:33 -0500

    tests with firebase made simpler

--- cypress.config.js
+++ cypress.config.js
@@ -61,29 +61,64 @@ module.exports = defineConfig({
   },
 
   e2e: {
+    // setupNodeEvents(on, config) {},
+    // specPattern: "cypress/e2e/**/*.cy.{js,ts}",
+    // plugins: ["@cypress/code-coverage"],
+    // video: false,
+    // videoUploadOnPasses: false,
+    // viewportWidth: 1920,
+    // viewportHeight: 1080,
     baseUrl: "http://localhost:3000",
-    async setupNodeEvents(on, config) {
-      require("@cypress/code-coverage/task")(on, config);
+    specPattern: "cypress/e2e/**/*.cy.{js,ts}",
+    setupNodeEvents(on, config) {
+      console.log("setupNodeEvents for components");
 
-      const file = config.env.configFile || "dev";
-      const envConfig = await getConfigurationByFile(file);
-      d;
-      const allConfig = merge({}, config, envConfig);
-      return allConfig;
-      dd;
+      // https://github.com/bahmutov/cypress-code-coverage
+      // eslint-disable-next-line global-require
+      require("@bahmutov/cypress-code-coverage/plugin")(on, config);
+      // eslint-disable-next-line global-require
+      require("@cypress/code-coverage/task")(on, config);
+      // eslint-disable-next-line global-require
+      on("file:preprocessor", require("@cypress/code-coverage/use-babelrc"));
+
+      return cypressFirebasePlugin(on, config, admin, {
+        projectId: "team-ate",
+      });
     },
 
-    specs: ["./e2e"],
+    env: {
+      codeCoverageTasksRegistered: true,
+    },
 
-    // NOTE: If not setting GCLOUD_PROJECT env variable, project can be set like so:
-    // return cypressFirebasePlugin(on, config, admin, { projectId: 'some-project' });
+    devServer: {
+      framework: "create-react-app",
+      bundler: "webpack",
+      webpackConfig: {
+        mode: "development",
+        devtool: false,
+        module: {
+          rules: [
+            // application and Cypress files are bundled like React components
+            // and instrumented using the babel-plugin-istanbul
+            // (we will filter the code coverage for non-application files later)
+            {
+              test: /\.js/,
+              exclude: /node_modules/,
+              use: {
+                loader: "babel-loader",
+                options: {
+                  presets: ["@babel/preset-env", "@babel/preset-react"],
+                  plugins: [
+                    // we could optionally insert this plugin
+                    // only if the code coverage flag is on
+                    "istanbul",
+                  ],
+                },
+              },
+            },
+          ],
+        },
+      },
+    },
   },
 });
-
-async function getConfigurationByFile(file) {
-  const pathToConfigFile = path.resolve(
-    "cypress/config",
-    `cypress.${file}.json`
-  );
-  return await fs.readJson(pathToConfigFile);
-}
