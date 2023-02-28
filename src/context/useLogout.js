670b15dd0e6274e2c5f9d994d2d4a93675c134f1
Mohammed <68031021+MohammedJava@users.noreply.github.com>
2023-02-28 09:42:56 -0500

    clean up function for hooks

--- src/context/useLogout.js
+++ src/context/useLogout.js
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { auth } from "../Firebase/firebase";
 import useAuthContext from "./useAuthContext";
 
 const useLogout = () => {
+  const [cancelled, setIsCancelled] = useState(false);
   const [error, setError] = useState(null);
   const [isPending, setIsPending] = useState(false);
   const { dispatch } = useAuthContext();
@@ -18,15 +19,23 @@ const useLogout = () => {
       // dispatch logout action
       dispatch({ type: "LOGOUT" });
 
+      if (!cancelled) {
+        setIsPending(false);
+      }
+
       // update state
       setIsPending(false);
       setError(null);
     } catch (err) {
-      console.log(err.message);
-      setError(err.message);
-      setIsPending(false);
+      if (!cancelled) {
+        console.log(err.message);
+        setError(err.message);
+        setIsPending(false);
+      }
     }
   };
+
+  useEffect(() => () => setIsCancelled(true), []);
 
   return { logout, error, isPending };
 };
