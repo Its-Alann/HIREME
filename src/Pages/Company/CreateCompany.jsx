2cc29428c7e46d722f112e6b3101e72c9774f4ac
RIGNITE <rck2001@hotmail.com>
2023-03-08 13:22:40 -0500

    fixed document ID for companies and recruiters

--- src/Pages/Company/CreateCompany.jsx
+++ src/Pages/Company/CreateCompany.jsx
@@ -4,7 +4,7 @@ import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import * as React from "react";
 import Container from "@mui/material/Container";
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, setDoc, doc } from "firebase/firestore";
 import Stack from "@mui/material/Stack";
 import { db } from "../../Firebase/firebase";
 
@@ -12,8 +12,15 @@ export const CreateCompany = () => {
   const [companyInformation, setCompanyInformation] = React.useState({
     name: "",
   });
+
   async function handleSubmit() {
-    await addDoc(collection(db, "companies"), companyInformation);
+    // await addDoc(collection(db, "companies", companyInformation.name), {
+    //   companyInformation,
+    // });
+
+    //Set document ID to company name
+    const newDocRef = doc(db, "companies", companyInformation.name);
+    await setDoc(newDocRef, companyInformation, { merge: true });
   }
   return (
     <Container maxWidth="md">
