ca137841df2b8a31e5eb64570cdabf79bf46f1de
Ryan Kim <rck2001@hotmail.com>
2023-03-08 00:47:51 -0500

    added mui style

--- src/Pages/Company/CreateCompany.jsx
+++ src/Pages/Company/CreateCompany.jsx
@@ -3,7 +3,9 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import * as React from "react";
+import Container from "@mui/material/Container";
 import { addDoc, collection } from "firebase/firestore";
+import Stack from "@mui/material/Stack";
 import { db } from "../../Firebase/firebase";
 
 export const CreateCompany = () => {
@@ -14,22 +16,36 @@ export const CreateCompany = () => {
     await addDoc(collection(db, "companies"), companyInformation);
   }
   return (
-    <Box>
-      <Typography>Company Creation</Typography>
-      <Typography>Company Name</Typography>
-      <TextField
-        required
-        id="TextField-CompanyName"
-        placeholder="Company Name"
-        variant="standard"
-        fullWidth
-        value={companyInformation.name}
-        onChange={(e) => setCompanyInformation({ name: e.target.value })}
-      />
-      <Button id="ButtonSave" onClick={() => handleSubmit()}>
-        Save
-      </Button>
-    </Box>
+    <Container maxWidth="md">
+      <Box sx={{ pt: 5 }}>
+        <Typography variant="h4" sx={{ pb: 2 }}>
+          Company Creation
+        </Typography>
+        <Stack spacing={2}>
+          <Box>
+            <Typography>Company Name</Typography>
+            <TextField
+              required
+              id="TextField-CompanyName"
+              placeholder="Company Name"
+              variant="standard"
+              fullWidth
+              value={companyInformation.name}
+              onChange={(e) => setCompanyInformation({ name: e.target.value })}
+            />
+          </Box>
+        </Stack>
+        <Button
+          variant="contained"
+          id="ButtonSave"
+          size="medium"
+          sx={{ mt: 2 }}
+          onClick={() => handleSubmit()}
+        >
+          Save
+        </Button>
+      </Box>
+    </Container>
   );
 };
 export default CreateCompany;
