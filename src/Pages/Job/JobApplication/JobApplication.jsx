21719d50617b42b6ddeae6f065cbe5e92c7b4c4b
Its-Alann <76508163+Its-Alann@users.noreply.github.com>
2023-03-09 22:04:11 -0500

    Finished job application styling + comments

--- src/Pages/Job/JobApplication/JobApplication.jsx
+++ src/Pages/Job/JobApplication/JobApplication.jsx
@@ -12,6 +12,7 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 import FileUpload from "../../../Components/FileUpload/FileUpload";
 
 const JobApplication = () => {
+  // Define the MUI theme to be used in the component
   const theme = createTheme({
     palette: {
       primary: { main: "#2B2F90" },
@@ -23,14 +24,15 @@ const JobApplication = () => {
     },
   });
 
+  // Define state variables for the form input fields and files to be uploaded
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
-
   const [resume, setResume] = useState(null);
   const [coverLetter, setCoverLetter] = useState(null);
   const [transcript, setTranscript] = useState(null);
 
+  // Event handler function to update the state variables when a file is selected for upload
   const onFileChange = (e, fileType) => {
     e.preventDefault();
     switch (fileType) {
@@ -54,152 +56,88 @@ const JobApplication = () => {
   };
 
   return (
+    // Apply the MUI theme to the component using the ThemeProvider component
     <ThemeProvider theme={theme}>
       <Grid
-        item
+        container
         sx={{
+          bgcolor: "white",
+          maxWidth: 1200,
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
-          maxWidth: 600,
+          textAlign: "center",
+          mx: "auto",
+          my: 10,
         }}
       >
+        {/* Page title */}
+        <Grid item md={12} sm={12} xs={12}>
+          <Typography variant="h3"> Your Application </Typography>
+        </Grid>
+
+        {/* Job title */}
+        <Grid item md={12} sm={12} xs={12}>
+          <Typography variant="h6">
+            You are applying for: Nuance Product Manager
+          </Typography>
+        </Grid>
+
+        {/* List of files to upload */}
+        <Grid item>
+          <List>
+            <ListItem disableGutters>
+              <ListItemText sx={{ mr: 5 }}> Resume </ListItemText>
+              <FileUpload
+                onFileChange={(e) => onFileChange(e, "resume")}
+                data-testid="upload-resume"
+              />
+            </ListItem>
+            <ListItem disableGutters>
+              <ListItemText sx={{ mr: 5 }}> Cover Letter </ListItemText>
+              <FileUpload
+                onFileChange={(e) => onFileChange(e, "coverLetter")}
+                data-testid="upload-coverLetter"
+              />
+            </ListItem>
+            <ListItem disableGutters>
+              <ListItemText sx={{ mr: 5 }}> Transcript </ListItemText>
+              <FileUpload
+                onFileChange={(e) => onFileChange(e, "transcript")}
+                data-testid="upload-transcript"
+              />
+            </ListItem>
+          </List>
+        </Grid>
+        <Grid item md={12} sm={12} xs={12}>
+          <TextField id="standard-basic" label="Email" variant="standard" />
+        </Grid>
+        <Grid item md={12} sm={12} xs={12}>
+          <TextField
+            id="standard-basic"
+            label="Phone Number"
+            variant="standard"
+          />
+        </Grid>
+        <Grid item md={12} sm={12} xs={12}>
+          <TextField id="standard-basic" label="Address" variant="standard" />
+        </Grid>
         <Grid
-          container
+          item
+          md={12}
+          sm={12}
+          xs={12}
           sx={{
-            
-            bgcolor: "white",
-            maxWidth: 600,
+            my: 5,
           }}
         >
-          <Grid
-            item
-            md={12}
-            sm={12}
-            xs={12}
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
+          <Button
+            variant="contained"
+            color="primary"
+            data-testid="submit-button"
           >
-            <Typography variant="h3"> Your Application </Typography>
-          </Grid>
-          <Grid
-            item
-            md={12}
-            sm={12}
-            xs={12}
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <Typography variant="h6">
-              You are applying for: Nuance Product Manager
-            </Typography>
-          </Grid>
-          <Grid item>
-            <List>
-              <ListItem disableGutters>
-                <ListItemText sx={{ mr: 5 }}> Resume </ListItemText>
-                <FileUpload
-                  onFileChange={(e) => onFileChange(e, "resume")}
-                  data-testid="upload-resume"
-                />
-              </ListItem>
-              <ListItem disableGutters>
-                <ListItemText sx={{ mr: 5 }}> Cover Letter </ListItemText>
-                <FileUpload
-                  onFileChange={(e) => onFileChange(e, "coverLetter")}
-                  data-testid="upload-coverLetter"
-                />
-              </ListItem>
-              <ListItem disableGutters>
-                <ListItemText sx={{ mr: 5 }}> Transcript </ListItemText>
-                <FileUpload
-                  onFileChange={(e) => onFileChange(e, "transcript")}
-                  data-testid="upload-transcript"
-                />
-              </ListItem>
-            </List>
-          </Grid>
-          {/* <Grid container md={12} sm={12} xs={12} spacing={5}>
-          <Grid item>
-            <TextField id="standard-basic" label="Email" variant="standard" />
-          </Grid>
-          <Grid item>
-            <TextField
-              id="standard-basic"
-              label="Phone Number"
-              variant="standard"
-            />
-          </Grid>
-        </Grid> */}
-          <Grid
-            item
-            md={12}
-            sm={12}
-            xs={12}
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <TextField id="standard-basic" label="Email" variant="standard" />
-          </Grid>
-          <Grid
-            item
-            md={12}
-            sm={12}
-            xs={12}
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <TextField
-              id="standard-basic"
-              label="Phone Number"
-              variant="standard"
-            />
-          </Grid>
-          <Grid
-            item
-            md={12}
-            sm={12}
-            xs={12}
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <TextField id="standard-basic" label="Address" variant="standard" />
-          </Grid>
-          <Grid
-            item
-            md={12}
-            sm={12}
-            xs={12}
-            sx={{
-              my: 5,
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <Button
-              variant="contained"
-              color="primary"
-              data-testid="submit-button"
-            >
-              Submit
-            </Button>
-          </Grid>
+            Submit
+          </Button>
         </Grid>
       </Grid>
     </ThemeProvider>
