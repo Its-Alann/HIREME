1cdc089999ad255ac51bd2ed608f0711e64f964a
Ryan Kim <rck2001@hotmail.com>
2023-03-15 15:17:15 -0400

    working job details
    - need to get company name
    - fix css

--- src/Pages/Candidate/ViewMyApp2.jsx
+++ src/Pages/Candidate/ViewMyApp2.jsx
@@ -26,7 +26,6 @@ import "../Job/Job.css";
 
 export const ViewMyApp2 = () => {
   const [jobs, setJobs] = React.useState([]);
-  const [companiesName, setCompaniesName] = React.useState({});
   const [myApplications, setMyApplications] = React.useState([]);
 
   const jobsPerPage = 5;
@@ -46,6 +45,28 @@ export const ViewMyApp2 = () => {
   //     setMyApplications(arrayOfJobIds);
   //   }
 
+  const getJobInformation = async (jobId) => {
+    console.log(jobId);
+    try {
+      const jobInformationSnapshot = await getDoc(doc(db, "jobs", jobId));
+      const jobInformationData = jobInformationSnapshot.data();
+      //console.log(jobInformationData);
+      const jobInformation = {
+        jobTitle: jobInformationData.title,
+        jobID: jobId,
+        companyID: jobInformationData.companyID,
+        deadline: jobInformationData.deadline,
+      };
+      console.log(jobInformation);
+      setMyApplications((prevApplications) => [
+        ...prevApplications,
+        jobInformation,
+      ]);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   // The alternative way is to fetch the entire collection
   // to the local machine, storing it in a list.
   // But I don't want to do it that way.
@@ -57,112 +78,15 @@ export const ViewMyApp2 = () => {
       doc(db, "applications", "billybob@gmail.com")
     );
     const applicationsData = applicationsSnapshot.data().jobs;
-
-    console.log(applicationsData);
-
-    // const arrayOfJobIds = [];
-    // const arrayOfStatus = [];
-    // applicationsData.forEach((item) => {
-    //   arrayOfJobIds.push(item.jobID);
-    //   arrayOfStatus.push(item.status);
-    // });
-
-    // console.log("this SHOULD BE AN ARRAY OF USER IDS", arrayOfJobIds);
-    // console.log("this SHOULD BE AN ARRAY OF USER STATUSES", arrayOfStatus);
-    // //setMyApplications(arrayOfJobIds);
-
-    // const initialJobsQuery = query(collection(db, "jobs"));
-
-    // const jobsSnapshot = await getDocs(initialJobsQuery);
-
-    // // if none document returned, skip
-    // if (jobsSnapshot.docs.length < 1) {
-    //   return;
-    // }
-
-    // const temp = [];
-    // jobsSnapshot.docs.forEach((document) => {
-    //   console.log("JOBBBBS", document);
-    //   console.log(arrayOfJobIds);
-    //   if (arrayOfJobIds.includes(document.id)) {
-    //     temp.push({
-    //       ...document.data(),
-    //       documentID: document.id,
-    //       status: "pending",
-    //     });
-    //   }
-    // });
-    // temp.reverse();
-    setJobs(applicationsData);
-  }
-
-  // const getJobTitle = async (jobID) => {
-  //   const jobsRef = doc(db, "jobs", jobID);
-  //   const docSnap = await getDoc(jobsRef);
-  //   const data = docSnap.data().title;
-  //   console.log("title", data);
-  //   return data.title;
-  // };
-
-  // const getCompany = async (jobID) => {
-  //   const jobsRef = doc(db, "jobs", jobID);
-  //   const docSnap = await getDoc(jobsRef);
-  //   const data = docSnap.data().companyID;
-  //   console.log("companyID", data);
-  // };
-
-  // const getLocation = async (jobID) => {
-  //   const jobsRef = doc(db, "jobs", jobID);
-  //   const docSnap = await getDoc(jobsRef);
-  //   const data = docSnap.data().location;
-  //   console.log("location", data);
-  // };
-
-  // const getDeadline = async (jobID) => {
-  //   const jobsRef = doc(db, "jobs", jobID);
-  //   const docSnap = await getDoc(jobsRef);
-  //   const data = docSnap.data().deadline;
-  //   console.log("deadline", data);
-  // };
-  const [title, setTitle] = React.useState("");
-  const [company, setCompany] = React.useState("");
-  const [location, setLocation] = React.useState("");
-  const [deadline, setDeadline] = React.useState("");
-
-  const getAllInfo = async (jobID) => {
-    const jobsRef = doc(db, "jobs", jobID);
-    const docSnap = await getDoc(jobsRef);
-
-    setTitle(docSnap.data().title);
-    setCompany(docSnap.data().companyID);
-    setLocation(docSnap.data().location);
-    setDeadline(docSnap.data().deadline);
-  };
-
-  // Get companies' name using the companyID of each Job
-  // And store them.
-  // If the companies' name has already been stored, skip
-  function getCompaniesName() {
-    const temp = companiesName;
-    jobs.forEach(async (job) => {
-      if (!temp[job.companyID]) {
-        temp[job.companyID] = "querying";
-        const companyRef = doc(db, "companies", job.companyID);
-        const companySnapshot = await getDoc(companyRef);
-        temp[job.companyID] = companySnapshot.data().name;
-        setCompaniesName({ ...temp });
-      }
-    });
+    await applicationsData.map((job) =>
+      Promise.all(getJobInformation(job.jobID))
+    );
+    console.log(myApplications);
   }
 
   useEffect(() => {
-    getCompaniesName();
-    console.log("STOPPPPPPP");
-  }, [jobs]);
-
-  useEffect(() => {
-    console.log("STOPPPPPPP");
-    getJobs();
+    console.log("STOPPPPPP");
+    Promise.all([getJobs()]);
   }, []);
 
   return (
@@ -172,16 +96,8 @@ export const ViewMyApp2 = () => {
           My Application
         </Typography>
         {/* {getJobTitle("4QwjqeYxPRuDw7fOnKBj")}; */}
-        {jobs.map((job) => {
+        {myApplications.map((job) => {
           const hello = "hello";
-
-          getAllInfo(job.jobID);
-          console.log("ALL INFO", title, company, deadline, location);
-          //   const jobTitle = getJobTitle(job.jobID);
-          // const jobTitle = getJobTitle(job.jobID);
-          // const company = getCompany(job.jobID);
-          // const location = getLocation(job.jobID);
-          // const deadline = getDeadline(job.jobID);
 
           // do this to show what is inside job
           // console.log(job);
@@ -190,7 +106,7 @@ export const ViewMyApp2 = () => {
             <Box sx={{ py: 1 }}>
               <Card variant="outlined">
                 <Box sx={{ m: 3 }}>
-                  <Typography variant="h4">{job.jobID}</Typography>
+                  <Typography variant="h4">{job.jobTitle}</Typography>
                   <Button
                     variant="contained"
                     sx={{
@@ -202,12 +118,7 @@ export const ViewMyApp2 = () => {
                     Remove
                   </Button>
                   <Typography>comp </Typography>
-
-                  {/* change to country and city */}
                   <Typography>{job.jobID}</Typography>
-
-                  {/* do we need to show company id? */}
-                  {/* <Typography>Company ID: {job.companyID}</Typography> */}
 
                   <Stack
                     direction="row"
@@ -215,13 +126,13 @@ export const ViewMyApp2 = () => {
                     alignItems="flex-end"
                     sx={{ pt: 2 }}
                   >
-                    {/* <Typography>
+                    <Typography>
                       Deadline:{" "}
                       {new Date(
-                        getDeadline(job.jobID).seconds * 1000 +
-                          getDeadline(job.jobID).nanoseconds / 1000000
+                        job.deadline.seconds * 1000 +
+                          job.deadline.nanoseconds / 1000000
                       ).toDateString()}
-                    </Typography> */}
+                    </Typography>
                   </Stack>
                 </Box>
               </Card>
