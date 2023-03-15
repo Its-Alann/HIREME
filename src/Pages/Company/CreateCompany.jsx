aec8094965959dd01707af0f3132358c029065c0
Aykchen <aliceykchen01@gmail.com>
2023-03-15 01:04:37 -0400

    Add logo and navigate to Home when submit

--- src/Pages/Company/CreateCompany.jsx
+++ src/Pages/Company/CreateCompany.jsx
@@ -3,17 +3,51 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import * as React from "react";
+import { useState } from "react";
 import Container from "@mui/material/Container";
 import { addDoc, collection } from "firebase/firestore";
 import Stack from "@mui/material/Stack";
-import { db } from "../../Firebase/firebase";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { useNavigate } from "react-router-dom";
+import { db, storage } from "../../Firebase/firebase";
 
 export const CreateCompany = () => {
+  const navigate = useNavigate();
+  const [url, setUrl] = useState("");
   const [companyInformation, setCompanyInformation] = React.useState({
     name: "",
+    logo: null, // new state for the uploaded logo file
   });
   async function handleSubmit() {
-    await addDoc(collection(db, "companies"), companyInformation);
+    // Upload the logo file to Firebase Storage
+    const storageRef = ref(
+      storage,
+      `/company-logo/${companyInformation.logo.name}`
+    );
+    await uploadBytes(storageRef, companyInformation.logo).then(() => {
+      getDownloadURL(storageRef)
+        // eslint-disable-next-line no-shadow
+        .then(async (url) => {
+          setUrl(url);
+          // Add the company information to the database
+          await addDoc(collection(db, "companies"), {
+            name: companyInformation.name,
+            logoPath: url, // Store the path to the uploaded logo file in the database
+          });
+        })
+        .catch((error) => {
+          console.log(error.message, "error getting the image url");
+        });
+    });
+    navigate("/");
+  }
+
+  // update the state with the selected logo file
+  function handleLogoChange(e) {
+    setCompanyInformation({
+      ...companyInformation,
+      logo: e.target.files[0],
+    });
   }
   return (
     <Container maxWidth="md">
@@ -31,8 +65,17 @@ export const CreateCompany = () => {
               variant="standard"
               fullWidth
               value={companyInformation.name}
-              onChange={(e) => setCompanyInformation({ name: e.target.value })}
+              onChange={(e) =>
+                setCompanyInformation({
+                  ...companyInformation,
+                  name: e.target.value,
+                })
+              }
             />
+          </Box>
+          <Box>
+            <Typography>Company Logo</Typography>
+            <input type="file" accept="image/*" onChange={handleLogoChange} />
           </Box>
         </Stack>
         <Button
