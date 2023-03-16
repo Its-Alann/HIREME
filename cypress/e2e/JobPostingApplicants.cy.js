0cb5c6ea4b03dc1ab639f64876b90b0b663dab3f
Ryan Kim <rck2001@hotmail.com>
2023-03-16 01:45:11 -0400

    completed tests

--- cypress/e2e/JobPostingApplicants.cy.js
+++ cypress/e2e/JobPostingApplicants.cy.js
@@ -18,16 +18,14 @@ describe("Login to test account", () => {
 
 describe("Display Job Posting", () => {
   it("visits the job listings page", () => {
-    cy.visit("http://localhost:3000/browseJobs");
+    cy.visit("http://localhost:3000/myJobs");
     cy.wait(1000);
   });
 
   it("clicks on job listing with applicants", () => {
-    cy.visit("http://localhost:3000/browseJobs");
+    cy.visit("http://localhost:3000/myJobs");
     cy.wait(1000);
-    cy.get(
-      ":nth-child(4) > .MuiPaper-root > .MuiBox-root > .css-qvcdic-MuiStack-root > :nth-child(2) > .link"
-    ).click();
+    cy.get("#Button-aYwmsGRNdWgosLt24YFD").click();
   });
 });
 
@@ -91,5 +89,47 @@ describe("Exit from popup window", () => {
       ":nth-child(2) > .MuiPaper-root > .MuiButtonBase-root > .MuiCardContent-root"
     ).click();
     cy.get(".css-1acyv19 > .MuiBox-root > .MuiButtonBase-root").click();
+  });
+});
+
+describe("Login to different test account", () => {
+  it("Logs into test account", () => {
+    cy.logout();
+    cy.visit("http://localhost:3000/login");
+    cy.get("#email").type("accountcreation@test.com");
+    cy.get("#password").type("test123");
+    cy.get("#email").focus();
+    cy.get(".MuiButton-contained").click();
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(1000);
+  });
+});
+
+describe("Remove a job", () => {
+  it("remove a job", () => {
+    cy.visit("http://localhost:3000/myJobs");
+    cy.get(`[data-cy="view"]`).first().click();
+    cy.get(".css-gmwslw-MuiStack-root > :nth-child(2) > :nth-child(2)").click();
+    cy.get(".MuiDialogActions-root > a > .MuiButtonBase-root").click();
+  });
+});
+
+describe("Create a new job", () => {
+  it("recreates a new job for the one deleted", () => {
+    cy.visit("http://localhost:3000/createJob");
+    // not a good fix
+    // we are waiting for firebase to give the id of the company.
+    // we can try cy.spy() on console log.
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(2000);
+    cy.get("#TextField-Title").type("Tester Job Title");
+    cy.get("#TextField-City").type("City");
+    cy.get("#TextField-Country").type("Country");
+    cy.get("#TextField-Description").type("Tester Job Description");
+    cy.get("#TextField-Requirement").type("Tester Job Requirement");
+    cy.get("#TextField-Benefits").type("Tester Job Benefits");
+    // somehow cannot get date picker by id
+    // cy.get("#DatePicker-Deadline").click();
+    cy.get("#Button-Save").click();
   });
 });
