a6706759f4af32f0e1a030bfc15ce239a0d680a3
Alan Vadlakunta <76508163+Its-Alann@users.noreply.github.com>
2023-04-01 02:31:57 -0400

    Fixed account creation test (#164)

--- cypress/e2e/users/AccountCreation.cy.js
+++ cypress/e2e/users/AccountCreation.cy.js
@@ -55,7 +55,7 @@ describe("Filling Up Forms", () => {
       ).type("H1H 1H1");
       cy.get(".MuiInputAdornment-root > .MuiButtonBase-root").click();
       cy.get(
-        ".MuiDayPicker-monthContainer > :nth-child(1) > :nth-child(4)"
+        ".MuiDayPicker-monthContainer > :nth-child(3) > :nth-child(4)"
       ).click();
     });
   });
