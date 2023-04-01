fa97aded10fcae09f489494a780bd000974e05ac
Ryan Kim <rck2001@hotmail.com>
2023-04-01 13:36:00 -0400

    Added pagination and fixed the background color issues

--- src/Pages/Network/Invitation/SentInvitation.jsx
+++ src/Pages/Network/Invitation/SentInvitation.jsx
@@ -3,14 +3,12 @@ import CssBaseline from "@mui/material/CssBaseline";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Grid from "@mui/material/Grid";
-import { getDoc, doc } from "firebase/firestore";
-import { onAuthStateChanged } from "firebase/auth";
 import PropTypes from "prop-types";
 import { Typography } from "@mui/material";
 import { SentInvitationCard } from "../../../Components/Network/SentInvitationCard";
-import { db, auth } from "../../../Firebase/firebase";
 import image2 from "../../../Assets/images/390image2.svg";
 
 const theme = createTheme();
@@ -23,11 +21,35 @@ export const SentInvitation = ({
   const [sentRequestsUserID, setSentRequestsUserID] = useState([]);
   const [allUsers, setAllUsers] = useState([]);
   const [currentUser, setCurrentUser] = useState([]);
+  const [showingSentInvites, setShowingSentInvites] = useState([]);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const pageSize = 15;
+
+  function paginate(arr, pageSizeSelected, pageNum) {
+    return arr.slice(
+      (pageNum - 1) * pageSizeSelected,
+      pageNum * pageSizeSelected
+    );
+  }
+
+  const nextPage = () => {
+    setPageNumber(pageNumber + 1);
+  };
+
+  const prevPage = () => {
+    setPageNumber(pageNumber - 1);
+  };
+
+  useEffect(() => {
+    setShowingSentInvites(paginate(sentRequestsUserID, pageSize, pageNumber));
+    console.log(showingSentInvites);
+  }, [pageNumber, sentRequestsUserID]);
 
   useEffect(() => {
     setSentRequestsUserID(sentInvitationsID);
     setCurrentUser(currentUserEmail);
-    console.log(currentUserEmail);
+    //console.log(currentUserEmail);
   }, [sentInvitationsID, currentUserEmail]);
 
   useEffect(() => {
@@ -39,45 +61,68 @@ export const SentInvitation = ({
       <ThemeProvider theme={theme}>
         <Container component="main" maxWidth="xxl" sx={{ m: 2 }}>
           <CssBaseline />
-          <Box justifyContent="center" alignItems="center" display="flex">
-            {sentRequestsUserID?.length > 0 && sentRequestsUserID != null ? (
-              <Grid
-                container
-                spacing={3}
-                display="flex"
-                justifyContent="center"
-                alignItems="center"
-              >
-                {sentRequestsUserID.map((userID) => (
-                  <Grid item data-cy="invitationsGrid">
-                    <SentInvitationCard
-                      allUserProfiles={allUsers}
-                      userID={userID}
-                      currentUser={currentUser}
-                    />
-                  </Grid>
-                ))}
-              </Grid>
-            ) : (
-              <>
-                <Box
-                  component="img"
+          <Stack>
+            <Box justifyContent="center" alignItems="center" display="flex">
+              {showingSentInvites?.length > 0 && showingSentInvites != null ? (
+                <Grid
+                  container
+                  spacing={3}
+                  display="flex"
                   justifyContent="center"
-                  sx={{
-                    width: 0.3,
-                    height: 0.3,
-                  }}
-                  src={image2}
-                  // alt="Trees"
-                />
-                <Stack alignItems="center" sx={{ ml: 1 }}>
-                  <Typography variant="h5">You have not sent</Typography>
-                  <Typography variant="h5">any invitations</Typography>
-                </Stack>
-              </>
-            )}
-            {/*The array will contain all the connected users*/}
-          </Box>
+                  alignItems="center"
+                >
+                  {showingSentInvites.map((userID) => (
+                    <Grid item data-cy="invitationsGrid">
+                      <SentInvitationCard
+                        allUserProfiles={allUsers}
+                        userID={userID}
+                        currentUser={currentUser}
+                      />
+                    </Grid>
+                  ))}
+                </Grid>
+              ) : (
+                <>
+                  <Box
+                    component="img"
+                    justifyContent="center"
+                    sx={{
+                      width: 0.3,
+                      height: 0.3,
+                    }}
+                    src={image2}
+                    // alt="Trees"
+                  />
+                  <Stack alignItems="center" sx={{ ml: 1 }}>
+                    <Typography variant="h5">You have not sent</Typography>
+                    <Typography variant="h5">any invitations</Typography>
+                  </Stack>
+                </>
+              )}
+              {/*The array will contain all the connected users*/}
+            </Box>
+            {sentRequestsUserID?.length > pageSize ? (
+              <Box sx={{ mt: 2 }}>
+                <Button
+                  id="Button-Previous"
+                  onClick={prevPage}
+                  disabled={pageNumber === 1}
+                >
+                  Prev
+                </Button>
+                <Button
+                  id="Button-Next"
+                  onClick={nextPage}
+                  disabled={
+                    pageNumber ===
+                    Math.ceil(sentRequestsUserID.length / pageSize)
+                  }
+                >
+                  Next
+                </Button>
+              </Box>
+            ) : null}
+          </Stack>
         </Container>
       </ThemeProvider>
     </div>
