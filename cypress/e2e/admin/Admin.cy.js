dba2b1315195cd28231b90d368a17cb2d9144818
Alan Vadlakunta <76508163+Its-Alann@users.noreply.github.com>
2023-04-06 13:27:06 -0400

    Messages with a user instead of an admin account

--- cypress/e2e/admin/Admin.cy.js
+++ cypress/e2e/admin/Admin.cy.js
@@ -1,3 +1,4 @@
+/* eslint-disable cypress/no-unnecessary-waiting */
 beforeEach(() => {
   cy.viewport(1920, 1080);
 });
@@ -11,47 +12,47 @@ describe("Login to test account", () => {
     cy.get("#email").focus();
     cy.get(".MuiButton-contained").click();
     // eslint-disable-next-line cypress/no-unnecessary-waiting
-    cy.wait(5000);
+    cy.wait(500);
   });
 });
 
 describe("Reports messages", () => {
   // should add a test where we go to messaging, report a message and then find that message in the admin page
-  it("writes 3 messages to the admin account", () => {
+  it("writes 3 messages to the second account", () => {
     //     //goes to messaging page and selects first convo (should be between Flagged Users account & admin15)
     cy.visit("http://localhost:3000/messaging");
     cy.wait(1000);
 
     cy.get(":nth-child(2) > .MuiListItemText-root > .MuiTypography-body1")
-      .should("have.text", "Admin15 Test")
+      .should("have.text", "Flagged Users 2")
       .click();
 
     //write 3 messages
     cy.get("#message-input").should("be.visible").type("toUnflag");
     cy.get('[data-testid="SendRoundedIcon"]').should("be.visible").click();
-    cy.wait(2000);
+    cy.wait(100);
     cy.get("#message-chats").last().should("contain", "toUnflag");
 
     cy.get("#message-input").should("be.visible").type("toWarn");
     cy.get('[data-testid="SendRoundedIcon"]').should("be.visible").click();
-    cy.wait(2000);
+    cy.wait(100);
     cy.get("#message-chats").last().should("contain", "toWarn");
 
     cy.get("#message-input").should("be.visible").type("toBlock");
     cy.get('[data-testid="SendRoundedIcon"]').should("be.visible").click();
-    cy.wait(2000);
+    cy.wait(100);
     cy.get("#message-chats").last().should("contain", "toBlock");
   });
 
-  it("Login to admin15 test account", () => {
+  it("Login to the second test account", () => {
     cy.logout();
     cy.visit("http://localhost:3000/login");
-    cy.get("#email").type("admin15@test.com");
+    cy.get("#email").type("flaggedusers2@test.com");
     cy.get("#password").type("test@123");
     cy.get("#email").focus();
     cy.get(".MuiButton-contained").click();
     // eslint-disable-next-line cypress/no-unnecessary-waiting
-    cy.wait(5000);
+    cy.wait(500);
   });
 
   it("reports the messages that the user has sent", () => {
@@ -63,7 +64,7 @@ describe("Reports messages", () => {
       .should("have.text", "Flagged User Test")
       .click();
 
-    cy.wait(500);
+    // cy.wait(500);
 
     // report last 3 messages
     cy.get(".messageOptions").last().invoke("show").click({ force: true });
@@ -72,22 +73,33 @@ describe("Reports messages", () => {
 
     cy.get(".messageOptions").eq(-2).invoke("show").click({ force: true });
     cy.get(
-      ":nth-child(6) > .MuiPaper-root > .MuiList-root > .MuiButtonBase-root"
+      ":nth-child(7) > .MuiPaper-root > .MuiList-root > .MuiButtonBase-root"
     ).click();
     cy.get('[data-testid="reportedBadge"]').should("be.visible");
 
     cy.get(".messageOptions").eq(-3).invoke("show").click({ force: true });
     cy.get(
-      ":nth-child(7) > .MuiPaper-root > .MuiList-root > .MuiButtonBase-root"
+      ":nth-child(8) > .MuiPaper-root > .MuiList-root > .MuiButtonBase-root"
     ).click();
     cy.get('[data-testid="reportedBadge"]').should("be.visible");
   });
 });
 
 describe("Admin Features", () => {
+  it("Login to admin15 test account", () => {
+    cy.logout();
+    cy.visit("http://localhost:3000/login");
+    cy.get("#email").type("admin15@test.com");
+    cy.get("#password").type("test@123");
+    cy.get("#email").focus();
+    cy.get(".MuiButton-contained").click();
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(500);
+  });
+
   it("clicks on the 3 buttons", () => {
     cy.visit("http://localhost:3000/admin/flaggedMessages");
-    cy.wait(1000);
+    cy.wait(500);
 
     // unflag a message
     cy.get(
@@ -96,7 +108,7 @@ describe("Admin Features", () => {
     cy.get(
       '.MuiDataGrid-row:nth-last-child(10) > [data-field="unflag"] > .MuiButtonBase-root'
     ).click();
-    cy.wait(1000);
+    cy.wait(500);
 
     // warn a user
     cy.get(
@@ -106,7 +118,7 @@ describe("Admin Features", () => {
     cy.get(
       '.MuiDataGrid-row:nth-last-child(10) > [data-field="warn"] > .MuiButtonBase-root'
     ).click();
-    cy.wait(5000);
+    cy.wait(500);
 
     // ban a user
     cy.get(
