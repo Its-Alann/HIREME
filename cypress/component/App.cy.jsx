55e5f77b3f52b90d71d2af55faa99b470b94e417
Mohammed <68031021+MohammedJava@users.noreply.github.com>
2023-04-08 14:15:25 -0400

    test actions

--- cypress/component/App.cy.jsx
+++ cypress/component/App.cy.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import App from "../../src/App";
 
 describe("<App />", () => {
-  it("it renders", () => {
+  it("renders", () => {
     // see: https://on.cypress.io/mounting-react
     cy.mount(<App />);
   });
