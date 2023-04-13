536fb5fef59b78793d40d04d9dcccbc390a89437
Yuchen Huang <YuchenHuang97@Gmail.com>
2023-04-12 20:12:33 -0400

    Update BrowseJobs.jsx

--- src/Pages/Job/BrowseJobs.jsx
+++ src/Pages/Job/BrowseJobs.jsx
@@ -159,64 +159,76 @@ export const BrowseJobs = () => {
           // console.log(job);
           return (
             // Create cards instead
-            <Box key={job.documentID} sx={{ py: 1 }}>
-              <Card variant="outlined">
-                <Box sx={{ m: 3 }}>
-                  <Stack direction="row" alignItems="center">
-                    <Box
-                      component="img"
-                      sx={{
-                        // objectFit: "cover",
-                        width: "6rem",
-                        height: "6rem",
-                        mr: 2,
-                      }}
-                      src={companiesLogo[job.companyID]}
-                    />
-                    <Box>
-                      <Typography variant="h4">{job.title}</Typography>
-                      <Typography>{companiesName[job.companyID]}</Typography>
-                      <Typography>{`${job.city}, ${job.country}`}</Typography>
-                    </Box>
-                  </Stack>
+            // <Box key={job.documentID} sx={{ py: 1 }}>
+            //   <Card variant="outlined">
+            //     <Box sx={{ m: 3 }}>
+            //       <Stack direction="row" alignItems="center">
+            //         <Box
+            //           component="img"
+            //           sx={{
+            //             // objectFit: "cover",
+            //             width: "6rem",
+            //             height: "6rem",
+            //             mr: 2,
+            //           }}
+            //           src={companiesLogo[job.companyID]}
+            //         />
+            //         <Box>
+            //           <Typography variant="h4">{job.title}</Typography>
+            //           <Typography>{companiesName[job.companyID]}</Typography>
+            //           <Typography>{`${job.city}, ${job.country}`}</Typography>
+            //         </Box>
+            //       </Stack>
 
-                  {/* do we need to show company id? */}
-                  {/* <Typography>Company ID: {job.companyID}</Typography> */}
+            //       {/* do we need to show company id? */}
+            //       {/* <Typography>Company ID: {job.companyID}</Typography> */}
 
-                  <Stack
-                    direction="row"
-                    justifyContent="space-between"
-                    alignItems="flex-end"
-                    sx={{ pt: 2 }}
-                  >
-                    {/* Added this button for candidate's view */}
-                    <Button
-                      variant="contained"
-                      size="medium"
-                      sx={{ my: 1 }}
-                      id={`Button-${job.documentID}`}
-                    >
-                      <Link
-                        to={`/viewJobPosting/${job.companyID}/${job.documentID}`}
-                        className="link"
-                        underline="none"
-                        style={{ textDecoration: "none" }}
-                      >
-                        {/* <Link to="/job/1"> */}
-                        View job
-                      </Link>
-                    </Button>
-                    <Typography>
-                      Deadline:{" "}
-                      {new Date(
-                        job.deadline.seconds * 1000 +
-                          job.deadline.nanoseconds / 1000000
-                      ).toDateString()}
-                    </Typography>
-                  </Stack>
-                </Box>
-              </Card>
-            </Box>
+            //       <Stack
+            //         direction="row"
+            //         justifyContent="space-between"
+            //         alignItems="flex-end"
+            //         sx={{ pt: 2 }}
+            //       >
+            //         {/* Added this button for candidate's view */}
+            //         <Button
+            //           variant="contained"
+            //           size="medium"
+            //           sx={{ my: 1 }}
+            //           id={`Button-${job.documentID}`}
+            //         >
+            //           <Link
+            //             to={`/viewJobPosting/${job.companyID}/${job.documentID}`}
+            //             className="link"
+            //             underline="none"
+            //             style={{ textDecoration: "none" }}
+            //           >
+            //             {/* <Link to="/job/1"> */}
+            //             View job
+            //           </Link>
+            //         </Button>
+            //         <Typography>
+            //           Deadline:{" "}
+            //           {new Date(
+            //             job.deadline.seconds * 1000 +
+            //               job.deadline.nanoseconds / 1000000
+            //           ).toDateString()}
+            //         </Typography>
+            //       </Stack>
+            //     </Box>
+            //   </Card>
+            // </Box>
+            <JobCard
+              key={`JobCard-${job.documentID}`}
+              companyID={job.companyID}
+              companyName={companiesName[job.companyID]}
+              jobID={job.documentID}
+              title={job.title}
+              city={job.city}
+              country={job.country}
+              deadlineSeconds={job.deadline.seconds}
+              deadlineNanoSeconds={job.deadline.nanoseconds}
+              logo={companiesLogo[job.companyID]}
+            />
           );
         })}
         <Button id="Button-Previous" onClick={() => getJobs(previousJobsQuery)}>
