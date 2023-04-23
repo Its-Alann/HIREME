912c88606144dfb59bb25d5e18a8e74edf46c6ff
XavierKMT <78317478+XavierKMT@users.noreply.github.com>
2023-04-22 22:56:05 -0400

    added favorite company and save job tests to company page

--- cypress/e2e/users/Company.cy.js
+++ cypress/e2e/users/Company.cy.js
@@ -12,7 +12,7 @@ describe("Login to test account", () => {
     cy.get("#email").focus();
     cy.get(".MuiButton-contained").click();
     // eslint-disable-next-line cypress/no-unnecessary-waiting
-    cy.wait(3000);
+    cy.wait(2000);
   });
 });
 
@@ -24,5 +24,39 @@ describe("Browse Jobs in company page", () => {
     // eslint-disable-next-line cypress/no-unnecessary-waiting
     cy.wait(500);
     cy.get("#Button-Previous").click();
+  });
+
+  it("Add and remove favourite", () => {
+    cy.visit("http://localhost:3000/companyPage/UJKcu7igvpu9mtnfvwII");
+    cy.wait(1000);
+    cy.get("body").then((body) => {
+      if (body.find('[data-testid="StarOutlineIcon"]').length > 0) {
+        cy.get('[data-testid="StarOutlineIcon"]').first().click();
+        cy.wait(1000);
+      }
+    });
+    cy.get("body").then((body) => {
+      if (body.find('[data-testid="StarIcon"]').length > 0) {
+        cy.get('[data-testid="StarIcon"]').first().click();
+        cy.wait(1000);
+      }
+    });
+  });
+
+  it("Save and unsave job", () => {
+    cy.visit("http://localhost:3000/companyPage/UJKcu7igvpu9mtnfvwII");
+    cy.wait(1000);
+    cy.get("body").then((body) => {
+      if (body.find('[data-cy="save-button"]').length > 0) {
+        cy.get('[data-cy="save-button"]').first().click();
+        cy.wait(500);
+      }
+    });
+    cy.get("body").then((body) => {
+      if (body.find('[data-cy="unsave-button"]').length > 0) {
+        cy.get('[data-cy="unsave-button"]').first().click();
+        cy.wait(500);
+      }
+    });
   });
 });
